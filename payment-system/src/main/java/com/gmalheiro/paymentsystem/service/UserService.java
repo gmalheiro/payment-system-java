27d47f955a9fdb816e711f3f3b64212efc76b4e9
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-03-29 00:22:39 -0300

    Created user UserService

---
+++ payment-system/src/main/java/com/gmalheiro/paymentsystem/service/UserService.java
@@ -0,0 +1,36 @@
+package com.gmalheiro.paymentsystem.service;
+
+import com.gmalheiro.paymentsystem.entity.User;
+import com.gmalheiro.paymentsystem.repository.UserRepository;
+import com.gmalheiro.paymentsystem.util.RandomString;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public User registerUser(User user){
+        if(userRepository.findByEmail(user.getEmail()) != null){
+            throw  new RuntimeException("This email already exists");
+        } else{
+            String encodedPassword = passwordEncoder.encode(user.getPassword());
+            user.setPassword(encodedPassword);
+
+            String randomCode = RandomString.generateRandomString(64);
+            user.setVerificationCode(randomCode);
+            user.setEnabled(false);
+
+            User savedUser = userRepository.save(user);
+
+            return  savedUser;
+        }
+    }
+
+}
