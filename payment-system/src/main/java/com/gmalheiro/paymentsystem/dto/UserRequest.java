d895719836195ec2a5c202f0a43a968083445252
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-03-29 22:57:47 -0300

    Added annotations to UserRequest

--- payment-system/src/main/java/com/gmalheiro/paymentsystem/dto/UserRequest.java
+++ payment-system/src/main/java/com/gmalheiro/paymentsystem/dto/UserRequest.java
@@ -1,8 +1,23 @@
 package com.gmalheiro.paymentsystem.dto;
 
 import com.gmalheiro.paymentsystem.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-public record UserRequest(String name, String email, String password) {
+public record UserRequest(
+        @NotNull(message = "Name cannot be null")
+        @NotBlank(message = "Name cannot be empty")
+        String name,
+        @NotNull(message = "Email cannot be null")
+        @NotBlank(message = "Email cannot be empty")
+        @Email
+        String email,
+        @NotNull(message = "Password cannot be null")
+        @NotBlank(message = "Password cannot be empty")
+        @Size(min = 8, message = "Password must contain at least 8 characters")
+        String password) {
 
 
     public User toModel(){
