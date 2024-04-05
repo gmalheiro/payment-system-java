34421940e0c9dd62dff85d3e1b43bdd2a7b88f77
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-04-04 23:27:10 -0300

    Update in controller added verify method

--- payment-system/src/main/java/com/gmalheiro/paymentsystem/controller/UserController.java
+++ payment-system/src/main/java/com/gmalheiro/paymentsystem/controller/UserController.java
@@ -6,11 +6,9 @@ import com.gmalheiro.paymentsystem.dto.UserRequest;
 import com.gmalheiro.paymentsystem.entity.User;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/user")
@@ -25,4 +23,14 @@ public class UserController {
         UserResponse userSaved = userService.registerUser(user);
         return  ResponseEntity.ok().body(userSaved);
     }
+
+    @GetMapping("/verify")
+    public String verifyUser(@Param("code") String code){
+        if(userService.verify(code)){
+            return "verify_success";
+        } else {
+            return "verify_fail";
+        }
+    }
+
 }
