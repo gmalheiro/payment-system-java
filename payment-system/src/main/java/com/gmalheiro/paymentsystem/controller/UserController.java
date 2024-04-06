550603708e119cb717e97038e444f3039befe5ac
Gabriel Malheiro <gabrielsilvamalheiro7@gmail.com>
2024-04-06 12:14:04 -0300

    Update in UserController to throw exception

--- payment-system/src/main/java/com/gmalheiro/paymentsystem/controller/UserController.java
+++ payment-system/src/main/java/com/gmalheiro/paymentsystem/controller/UserController.java
@@ -4,11 +4,14 @@ import com.gmalheiro.paymentsystem.dto.UserResponse;
 import com.gmalheiro.paymentsystem.service.UserService;
 import com.gmalheiro.paymentsystem.dto.UserRequest;
 import com.gmalheiro.paymentsystem.entity.User;
+import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
 
 @RestController
 @RequestMapping("/user")
@@ -18,7 +21,7 @@ public class UserController {
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserRequest userRequest){
+    public ResponseEntity<UserResponse> registerUser(@RequestBody @Valid UserRequest userRequest)   throws MessagingException, UnsupportedEncodingException {
         User user = userRequest.toModel();
         UserResponse userSaved = userService.registerUser(user);
         return  ResponseEntity.ok().body(userSaved);
